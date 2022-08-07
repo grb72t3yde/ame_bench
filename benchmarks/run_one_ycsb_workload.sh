ea2cf1bdb1ca34bd7239cfcf008a49bf4fb2180d
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2022-08-07 08:14:19 +0000

    Append run_with_ame suffix to YCSB run output log file
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- benchmarks/run_one_ycsb_workload.sh
+++ benchmarks/run_one_ycsb_workload.sh
@@ -3,10 +3,13 @@
 SUBMODULES_PATH="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )/../submodules")"
 port_num="$1"
 
+run_with_ame="with_ame"
+#run_with_ame="without_ame"
+
 cd ${SUBMODULES_PATH}/memcached
 ./memcached -m 16384 -p ${port_num} &
 
 cd ${SUBMODULES_PATH}/YCSB
 ./bin/ycsb load memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1:${port_num}" && \
-    ./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1:${port_num}" > outputRun_${port_num}.txt
+    ./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1:${port_num}" > outputRun_${port_num}_${run_with_ame}.txt
 
