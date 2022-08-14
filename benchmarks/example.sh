2b868ac712af05251818d390420006a7ae627652
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2022-08-14 09:38:00 +0000

    script: add delays to make memcached programs start at different timings
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- benchmarks/example.sh
+++ benchmarks/example.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 nr_memcached_ins=2
-
 default_port=11211
 
 for (( i = 0; i < ${nr_memcached_ins}; ++i ))
 do
     bash ./run_one_ycsb_workload.sh $((default_port + i)) &
+
+    # delay
+    min=20
+    sec=0
+    while [ $min -gt 0 ]
+    do
+        while [ $sec -gt 0 ]
+        do
+            echo -ne "$min:$sec\033[O\r"
+            let "sec=sec-1"
+            sleep 1
+        done
+        sec=59
+        let "min=min-1"
+    done
 done
 
 
