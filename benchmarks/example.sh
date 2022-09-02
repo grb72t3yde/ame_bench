b01fdedeb3b44d06b47b4b04cb12b96a44ce4d86
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2022-09-02 11:31:11 +0000

    Parameterize the YCSB workload types and the interval between two workloads
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- benchmarks/example.sh
+++ benchmarks/example.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-nr_memcached_ins=5
+nr_memcached_ins=2
 default_port=11211
 
-# start the logger
-bash mem_usage_logger.sh &
+interval_hours=(1 0)
+interval_minutes=(0 0)
+interval_seconds=(0 0)
+workloads=('a' 'b')
 
 # start memcached instances
 for (( i = 0; i < ${nr_memcached_ins}; ++i ))
 do
-    bash ./run_one_ycsb_workload.sh $((default_port + i)) &
+    bash ./run_one_ycsb_workload.sh $((default_port + i)) ${workloads[$i]} &
 
     # delay
-    bash ./count_down_timer.sh 0 10 0
+    bash ./count_down_timer.sh ${interval_hours[$i]} ${interval_minutes[$i]} ${interval_seconds[$i]}
 done
-
 
