2fc9c1d03768653946f2080b5c95574c4f7272c9
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2022-12-21 08:56:42 +0000

    run_one_ycsb_workload.sh: Add configurable arguments
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- benchmarks/run_one_ycsb_workload.sh
+++ benchmarks/run_one_ycsb_workload.sh
@@ -3,19 +3,21 @@
 SUBMODULES_PATH="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )/../submodules")"
 SCRIPT_PATH="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )")"
 port_num="$1"
+workload="$2"
+memory_usage=32768
 target_program="memcached"
 
-#run_with_ame="with_ame"
-run_with_ame="without_ame"
+run_with_ame="with_ame"
+#run_with_ame="without_ame"
 
 # create a memcached instance
 cd ${SUBMODULES_PATH}/memcached
-./memcached -m 65536 -p ${port_num} &
+./memcached -m ${memory_usage} -p ${port_num} > memcached_output${port_num} &
 
 # run YCSB workload
 cd ${SUBMODULES_PATH}/YCSB
-./bin/ycsb load memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1:${port_num}" && \
-    ./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1:${port_num}" > outputRun_${port_num}_${run_with_ame}.txt
+./bin/ycsb load memcached -s -P workloads/workload${workload} -p "memcached.hosts=127.0.0.1:${port_num}" && \
+    ./bin/ycsb run memcached -s -P workloads/workload${workload} -p "memcached.hosts=127.0.0.1:${port_num}" > outputRun_${port_num}_${run_with_ame}.txt
 
 # kill the memcached instance
 cd $SCRIPT_PATH
